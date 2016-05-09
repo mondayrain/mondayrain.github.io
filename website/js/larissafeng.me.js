c809ec30e8ea1afd56b6d31fc389f5e54e319db2
Larissa Feng <larissa.feng@shopify.com>
2016-05-08 21:15:12 -0400

    Added teletyping to landing

---
+++ website/js/larissafeng.me.js
@@ -0,0 +1,9 @@
+/* ALL JS FUNCTIONALITY FOR MAIN SITE */
+
+$( function() {
+    $( '#type-text' ).teletype( {
+        text: [ 'programmer', 'pythonista', 'OSS enthusiast', 'student' ],
+        typeDelay: 0,
+        backDelay: 10
+    } );
+} );
