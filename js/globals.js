8de4b155db57a1ccd9da71c0b640861f4190cf44
Larissa Feng <lfeng@wealthsimple.com>
2018-08-25 13:35:29 -0400

    Update reading list

--- js/globals.js
+++ js/globals.js
@@ -11,6 +11,7 @@ $( function() {
             "current_year": {
                 "title": "2018",
                 "list": [
+                    "A Thousand Years of Good Prayers, Yiyun Li",
                     "Disgrace, J.M. Coetzee",
                     "All the Light We Cannot See, Anthony Doerr",
                     "An Artist of the Floating World, Kazuo Ishiguro",
